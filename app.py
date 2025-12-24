ee724fd7cfcf20f7d4ef9b90b3551dbc8be07fd1
Armalek <arabmalek635@gmail.com>
2025-12-24 23:23:20 +0100

    Add requirements.txt for Render deployment

--- app.py
+++ app.py
@@ -398,5 +398,4 @@ def tp4_rlf_draw():
 
 if __name__ == '__main__':
    port = int(os.environ.get("PORT", 5000))
-   app.run(debug=True)
-    app.run(host='0.0.0.0', port=port)
+   app.run(host='0.0.0.0', port=port)
